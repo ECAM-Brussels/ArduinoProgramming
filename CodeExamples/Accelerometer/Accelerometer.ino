4db965000ef6367c94b76be2c1bf2d2efb71c659
Sébastien Combéfis <seb478@gmail.com>
2016-05-29 11:37:05 +0200

    CodeExamples: Add device detection to Accelerometer example

--- CodeExamples/Accelerometer/Accelerometer.ino
+++ CodeExamples/Accelerometer/Accelerometer.ino
@@ -1,16 +1,18 @@
 // Accelerometer.ino
 // Author: Sébastien Combéfis
-// Version: May 27, 2016
+// Version: May 29, 2016
 
 #include <Wire.h>
 
 #define KEYES_345_ADDRESS   (0x53)
 
+#define KEYES_REG_DEVID     (0x00)
 #define KEYES_REG_POWER_CTL (0x2D)
 #define KEYES_REG_DATAX0    (0x32)
 #define KEYES_REG_DATAY0    (0x34)
 #define KEYES_REG_DATAZ0    (0x36)
 
+// Write a 1-byte value into a register
 void writeRegister(uint8_t reg, uint8_t value) {
   Wire.beginTransmission(KEYES_345_ADDRESS);
   Wire.write(reg);
@@ -18,6 +20,7 @@ void writeRegister(uint8_t reg, uint8_t value) {
   Wire.endTransmission();
 }
 
+// Read a 1-byte value from a register
 int8_t readRegister(uint8_t reg) {
   Wire.beginTransmission(KEYES_345_ADDRESS);
   Wire.write(reg);
@@ -26,6 +29,7 @@ int8_t readRegister(uint8_t reg) {
   return Wire.read();
 }
 
+// Read a 2-byte value from a register
 int16_t readRegister16(uint8_t reg) {
   Wire.beginTransmission(KEYES_345_ADDRESS);
   Wire.write(reg);
@@ -38,8 +42,17 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
   
+  // Detect the accelerometer
+  char output[512];
+  uint8_t devid = readRegister(KEYES_REG_DEVID);
+  if (devid != 0xe5) {
+    Serial.write("Failed to find the device!\n");
+    exit(-1);
+  }
+  
   // Activate measurement mode
   writeRegister(KEYES_REG_POWER_CTL, 0x08);
+  Serial.write("Device detected, starting to measure!\n");
 }
 
 void loop() {
