01dbe1dacca1355cc81d93a3d51fd87b685b41db
Sébastien Combéfis <seb478@gmail.com>
2017-02-28 20:43:40 +0100

    CodesExample: Add automatic calibration to accelerometer example

--- CodeExamples/Accelerometer/Accelerometer.ino
+++ CodeExamples/Accelerometer/Accelerometer.ino
@@ -1,16 +1,20 @@
 // Accelerometer.ino
 // Author: Sébastien Combéfis
-// Version: May 29, 2016
+// Version: February 28, 2017
 
 #include <Wire.h>
 
-#define KEYES_345_ADDRESS   (0x53)
+#define KEYES_345_ADDRESS      (0x53)
 
-#define KEYES_REG_DEVID     (0x00)
-#define KEYES_REG_POWER_CTL (0x2D)
-#define KEYES_REG_DATAX0    (0x32)
-#define KEYES_REG_DATAY0    (0x34)
-#define KEYES_REG_DATAZ0    (0x36)
+#define KEYES_REG_DEVID        (0x00)
+#define KEYES_REG_POWER_CTL    (0x2D)
+#define KEYES_REG_DATA_FORMAT  (0x31)
+#define KEYES_REG_OFSX         (0x1E)
+#define KEYES_REG_OFSY         (0x1F)
+#define KEYES_REG_OFSZ         (0x20)
+#define KEYES_REG_DATAX0       (0x32)
+#define KEYES_REG_DATAY0       (0x34)
+#define KEYES_REG_DATAZ0       (0x36)
 
 // Write a 1-byte value into a register
 void writeRegister(uint8_t reg, uint8_t value) {
@@ -43,16 +47,38 @@ void setup() {
   Wire.begin();
   
   // Detect the accelerometer
-  char output[512];
   uint8_t devid = readRegister(KEYES_REG_DEVID);
   if (devid != 0xe5) {
     Serial.write("Failed to find the device!\n");
     exit(-1);
   }
   
+  // Set the sensitivity to +/-2g
+  Serial.write("Device detected, setting sensitivity to +/-2g...\n");
+  writeRegister(KEYES_REG_DATA_FORMAT, 0);
+  
+  // Calibrate the accelerometer
+  Serial.write("Calibrating the device...\n");
+  writeRegister(KEYES_REG_OFSX, 0);
+  writeRegister(KEYES_REG_OFSY, 0);
+  writeRegister(KEYES_REG_OFSZ, 0);
+
+  int avx = 0, avy = 0, avz = 0;
+  int N = 10;
+  for (int i = 0; i < N; i++) {
+    avx += readRegister16(KEYES_REG_DATAX0);
+    avy += readRegister16(KEYES_REG_DATAY0);
+    avz += readRegister16(KEYES_REG_DATAZ0);
+    delay(1000);
+  }
+  
+  writeRegister(KEYES_REG_OFSX, -(avx / N) / 4);
+  writeRegister(KEYES_REG_OFSY, -(avy / N) / 4);
+  writeRegister(KEYES_REG_OFSZ, -(avz / N) / 4);
+  
   // Activate measurement mode
   writeRegister(KEYES_REG_POWER_CTL, 0x08);
-  Serial.write("Device detected, starting to measure!\n");
+  Serial.write("Starting to measure!\n");
 }
 
 void loop() {
